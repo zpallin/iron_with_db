d61985d4e6066bd4c62118c2710e15b833fef662
zpallin <zpallin@gmail.com>
2016-10-21 08:09:13 -0700

    Okay, so I boiled it down a lot: DatabaseConfig struct
    The database config struct contains configuration for a database
    it will get more helper functions, but maybe not.
    
    It's designed to be handled at the developer's whim.

---
+++ src/db/mod.rs
@@ -0,0 +1,70 @@
+
+//! db module for iron_with_db
+//!
+//! ## Overview
+//!
+//! This module provides some tools for handling configuration input, 
+//! designed primarily with databases in mind.
+//!
+//! - zpallin
+//!
+
+use iron::typemap::Key;
+
+pub struct DatabaseConfig {
+
+    /// name of the database in the server
+    pub dbname: String,
+
+    /// hostname
+    pub hostname: String,
+
+    /// port
+    pub port: u16,
+}
+
+impl DatabaseConfig {
+
+    pub fn new(database_url: &str) -> DatabaseConfig {
+
+        // get the dbname
+        let split = database_url.split("/").collect::<Vec<&str>>();
+        let dbname = split[1].to_owned();
+
+        // get the hostname
+        let split = split[0].split(":").collect::<Vec<&str>>();
+        let hostname = split[0].to_owned();
+
+        // get the port
+        let port: u16 = split[1].to_owned().parse().ok().expect("DbConfig: Wanted a number");
+
+        DatabaseConfig
+        {
+            dbname: dbname,
+            hostname: hostname,
+            port: port,
+        }
+    }
+
+    pub fn display(&self) {
+        
+        println!(
+            "DatabaseConfig {{\n    \
+            dbname: {},\n    \
+            hostname: {},\n    \
+            port: {},\n\
+            }}",
+            self.dbname,
+            self.hostname,
+            self.port,
+        );
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct DatabaseConfigKey;
+
+impl Key for DatabaseConfigKey {
+    type Value = DatabaseConfig;
+}
+
--- src/main.rs
+++ src/main.rs
@@ -4,81 +4,31 @@ extern crate mongodb;
 extern crate iron_with_db;
 
 use iron_with_db::*;
+use iron_with_db::db::{DatabaseConfig, DatabaseConfigKey};
 use bson::Bson;
 use mongodb::{Client, ThreadedClient};
 use mongodb::db::ThreadedDatabase;
 
 fn route_handler(req: &mut Request) -> IronResult<Response> {
-    let arc = req.get::<Read<AppConfigKey>>().unwrap();
-    let app = arc.as_ref();
+    let arc = req.get::<Read<DatabaseConfigKey>>().unwrap();
+    let dbConf = arc.as_ref();
 
-    app.display();
+    dbConf.display();
 
     let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
     Ok(Response::with((status::Ok, *query)))
 }
 
-struct DbConfig {
-
-    /// name of the database in the server
-    dbname: String,
-
-    /// hostname 
-    hostname: String,
-
-    /// port
-    port: u16,
-}
-
-impl DbConfig {
-
-    fn new(database_url: &str) -> DbConfig {
-    
-        // get the dbname
-        let split = database_url.split("/").collect::<Vec<&str>>();
-        let dbname = split[1].to_owned();
-        
-        // get the hostname
-        let split = split[0].split(":").collect::<Vec<&str>>();
-        let hostname = split[0].to_owned();
-
-        // get the port
-        let port: u16 = split[1].to_owned().parse().ok().expect("DbConfig: Wanted a number");
-
-        DbConfig {
-            dbname: dbname,
-            hostname: hostname,
-            port: port,
-        }
-    }
-
-    /// obtain connection to the db
-    fn get_conn(&self) -> Client {
-
-        Client::connect(&self.hostname, self.port)
-            .ok().expect("Failed to initialize mongo client")
-    }
-}
-
-#[derive(Copy, Clone)]
-struct DbConf;
-impl Key for DbConf {
-    type Value = DbConfig;
-}
-
 fn main() {
 
-    let app = AppConfig::default();
-    let dbConf = DbConfig::new(&app.database_url);
+    let dbConf = DatabaseConfig::new("localhost:27017/test");
 
     let mut router = Router::new();
     router.get("/", route_handler, "index");
     router.get("/:query", route_handler, "query");
 
-
     let mut config = Chain::new(router);
-    config.link(Read::<AppConfigKey>::both(app));
-    config.link(Read::<DbConf>::both(dbConf));
+    config.link(Read::<DatabaseConfigKey>::both(dbConf));
 
     Iron::new(config).http("localhost:3000").unwrap();
 }
