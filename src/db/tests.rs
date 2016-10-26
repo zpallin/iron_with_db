255fd5c312f9de963316ab27d94d8e2a4c278de6
zpallin <zpallin@gmail.com>
2016-10-25 23:20:50 -0700

    Fix test errors

--- src/db/tests.rs
+++ src/db/tests.rs
@@ -1,13 +1,13 @@
 
 #[cfg(test)]
 #[test]
-fn test_DatabaseConfig() {
+fn test_database_config() {
     use db::DatabaseConfig;
     
-    let dbConf = DatabaseConfig::new("localhost:27017/test");
+    let db_conf = DatabaseConfig::new("localhost:27017/test");
 
-    assert_eq!("localhost", dbConf.hostname);
-    assert_eq!("test", dbConf.dbname);
-    assert_eq!(27017 as u16, dbConf.port);
+    assert_eq!("localhost", db_conf.hostname);
+    assert_eq!("test", db_conf.dbname);
+    assert_eq!(27017 as u16, db_conf.port);
 }
 
