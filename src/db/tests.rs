bf5a3a1959ef3fce83e27d98894f33368147b4ec
zpallin <zpallin@gmail.com>
2016-10-30 00:36:19 -0700

    Add test for display function which returns a String instead of printing, now

--- src/db/tests.rs
+++ src/db/tests.rs
@@ -1,5 +1,6 @@
 
 #[cfg(test)]
+
 #[test]
 fn test_database_config() {
     use db::DatabaseConfig;
@@ -9,5 +10,20 @@ fn test_database_config() {
     assert_eq!("localhost", db_conf.hostname);
     assert_eq!("test", db_conf.dbname);
     assert_eq!(27017 as u16, db_conf.port);
+}
+
+#[test]
+fn test_database_config_display() {
+    use db::DatabaseConfig;
+
+    let db_conf = DatabaseConfig::new("localhost:27017/test");
+
+    let expected = "DatabaseConfig {\n    \
+                    dbname: test,\n    \
+                    hostname: localhost,\n    \
+                    port: 27017,\n}";
+
+    assert_eq!(db_conf.display(), expected);
+    
 }
 
