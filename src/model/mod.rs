c96f82ede2b8f6b6d606af99130ad3d1825214bc
zpallin <zpallin@gmail.com>
2016-10-30 23:12:43 -0700

    Adding scaffolding for model library

---
+++ src/model/mod.rs
@@ -0,0 +1,21 @@
+
+//!
+//! models for iron_with_db
+//!
+//! ## Overview
+//!
+//! Models are used in order to create a code representation of data. Iron does not come with
+//! models, or MVC, built in, so iron_with_db will have one overlaying it instead.
+//!
+//! -zpallin
+//!
+
+
+/// Model trait for functionality assoiated with traits
+pub trait Model {
+    fn new() -> Model {
+        
+    }
+}
+
+/// 
